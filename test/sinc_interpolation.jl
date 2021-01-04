c30b0492cce737b34c1b64b62c4378c87d1f99f7
roflmaostc <fxw+git@mailbox.org>
2021-01-04 19:42:17 +0100

    Add testcases for hermitian

--- test/sinc_interpolation.jl
+++ test/sinc_interpolation.jl
@@ -27,6 +27,83 @@
 end
 
 
+@testset "make hermitian" begin
+    a = FFTInterpolations.make_hermitian([1im 2.0; 3.0im 4.0im; 5.0 6.0im])
+
+    ã = [0.0+0.5im  2.0+0.0im  2.5-0.0im; 0.0+1.5im  0.0+4.0im  0.0-1.5im; 2.5+0.0im  0.0+6.0im  0.0-0.5im]
+
+    @test a == ã
+    
+    a = FFTInterpolations.make_hermitian([1.0; 2.0; 3.0im])
+    ã = [1.0; 2.0; 3.0im]
+    @test a == ã
+
+
+    a = FFTInterpolations.make_hermitian([1.0+1.0im; 2.0; 3.0; 3.0im])
+    ã = [0.5+0.5im; 2.0; 3.0; 3.0im; 0.5-0.5im]
+    @test a == ã
+
+    function test_sum(s)
+        a = randn(s)
+        ã = FFTInterpolations.make_hermitian(a)
+        @test sum(a) ≈ sum(ã)
+    end    
+    test_sum((12, 3, 12, 11, 1))
+    test_sum((123, 312))
+    test_sum((1))
+    test_sum((2))
+    test_sum((100, 101))
+    test_sum((101, 101))
+    test_sum((101, 100))
+    test_sum((101, 101))
+
+    
+
+    function test_symmetry(s)
+        a = randn(s) 
+        b = FFTInterpolations.make_hermitian(a)
+
+        for k1 = 1:size(b)[1]
+            for k2 = 1:size(b)[2]
+                for k3 = 1:size(b)[3]
+                    if k1 != 1 && k2 != 1 && k3 != 1 
+                        continue
+                    end
+                    @test b[k1, k2, k3] ≈ 
+                        conj(b[size(b)[1] + 1 - k1, size(b)[2] + 1 - k2, size(b)[3] + 1 - k3])
+                end
+            end
+        end
+    end            
+    
+    test_symmetry((2,2,2))
+    test_symmetry((1,1,1))
+    test_symmetry((4,4,4))
+    test_symmetry((12,8,12))
+
+    function test_symmetry_4D(s)
+        a = randn(s) 
+        b = FFTInterpolations.make_hermitian(a)
+
+        for k1 = 1:size(b)[1]
+            for k2 = 1:size(b)[2]
+                for k3 = 1:size(b)[3]
+                    for k4 = 1:size(b)[4]
+                        if k1 != 1 && k2 != 1 && k3 != 1 && k4 != 1
+                            continue
+                        end
+                        @test b[k1, k2, k3, k4] ≈ conj(b[size(b)[1] + 1 - k1, size(b)[2] + 1 - k2, size(b)[3] + 1 - k3, size(b)[4] + 1 - k4])
+                    end
+                end
+            end
+        end
+    end            
+
+    test_symmetry_4D((2,4, 6, 8))
+
+
+end
+
 @testset "downsampe" begin
     function test_downsample(N_low, N)
 	    x_min = 0.0
