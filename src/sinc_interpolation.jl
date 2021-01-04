253cd821036561eea4bf66b00795b98ad8535dcb
roflmaostc <fxw+git@mailbox.org>
2021-01-04 18:58:56 +0100

    Add make_hermitian function to make arrays hermite

--- src/sinc_interpolation.jl
+++ src/sinc_interpolation.jl
@@ -1,5 +1,8 @@
 export sinc_interpolate, sinc_interpolate_sum
 
+using Base.Cartesian
+using GeneralizedGenerated
+
 """
     sinc_interpolate(arr, new_size)
 
@@ -30,18 +33,115 @@ julia> sinc_interpolate([1.0  2.0; 3.0 4.0], (4,4))
 ```
 """
 function sinc_interpolate(arr::AbstractArray{T}, new_size) where T<:Complex
+    @assert new_size ≥ size(arr)
+
+    # go to fourier space
     arr_f = fftshift(fft(arr))
+    # create fourier space new array
     out_f = zeros(eltype(arr_f), new_size)
+    # set the old array into the new 0-padded array
     center_set!(out_f, arr_f)
+    # go back to real space and apply proper value scaling
     out = ifft(ifftshift(out_f)) ./ length(arr) .* length(out_f)
     return out
 end
 
+ # for real arrays, take real part due to numerical inaccuracies
 function sinc_interpolate(arr::AbstractArray{T}, new_size) where T<:Real
     # array of same shape but with Complex element type
     arr_nt = Complex.(arr) 
     return real(sinc_interpolate(arr_nt, new_size))
 end
+
+
+"""
+    make_hermitian(arr)
+
+Takes an array `arr` and appends rows, cols, ... if necessary
+so that `arr` is a hermitian array which preserves Parseval's theorem.
+
+# Examples
+```julia-repl
+julia> FFTInterpolations.make_hermitian([1.0 2.0])
+1×3 Array{Float64,2}:
+ 0.5  2.0  0.5
+
+julia> FFTInterpolations.make_hermitian([1.0 2.0; 3.0 4.0])
+3×3 Array{Float64,2}:
+ 0.5  1.0  0.0
+ 1.5  4.0  1.5
+ 0.0  1.0  0.5
+
+julia> FFTInterpolations.make_hermitian([1im 2.0; 3.0im 4.0im; 5.0 6.0im])
+3×3 Array{Complex{Float64},2}:
+ 0.0+0.5im  2.0+0.0im  2.5-0.0im
+ 0.0+1.5im  0.0+4.0im  0.0-1.5im
+ 2.5+0.0im  0.0+6.0im  0.0-0.5im
+```
+"""
+@gg function make_hermitian(arr::AbstractArray{T, N}) where {T, N}
+    N2 = N - 1
+    quote 
+        # if the size is odd, we need to add a slice to that dimension
+        # at this slice we then fill new values, so that `arr` is hermitian
+        mf(x) = x % 2 == 0 ? x +1 : x
+        size_new = map(mf, size(arr))
+        # new array with new size 
+        arr_new = zeros(eltype(arr), size_new)
+        
+        # fill the new array with the old one 
+        same_ind = map(x -> 1:x, size(arr))
+        arr_new[same_ind...] = arr 
+        
+        # copy again to have a out array (might be necessary because of reassigning)
+        arr_out = copy(arr_new)
+        
+        # now modify values at the edges to get hermitian property
+        # and Parsveval's theorem correct
+        for d = 1:$N
+            # @show d
+            # special case when singleton dimension 
+            if size(arr_new)[d] == 1 || size(arr)[d] % 2 == 1
+                continue
+            end
+            
+            # the N2 dimensional loops, loop over this
+            iter_nloops = map(x -> 1:x, collect(size_new))
+            # delete at dimension d because we have only N2 dimensions to loop over
+            deleteat!(iter_nloops, d)
+            
+            # @show iter_nloops
+            @nloops $N2 i (k -> iter_nloops[k]) begin
+                # indices to access the left slice
+                # left slice is the existing slice which will be copied
+                inds_l = collect(@ntuple $N2 i)
+                insert!(inds_l, d, 1)
+                
+                # right slice which will be copied from the left slice
+                inds_r = similar(inds_l)
+                
+                # because of the hermitian property, we reverse the slices
+                for j in 1:length(inds_r)
+                    # in this case, we want to get the right hand slice
+                    if j == d
+                        inds_r[j] = size_new[j]
+                    else
+                        inds_r[j] = size_new[j] - inds_l[j] + 1
+                    end
+                end
+                
+                # assign the complex conjugate 
+                arr_out[inds_l...] = 0.5 * arr_new[inds_l...]
+                arr_out[inds_r...] = 0.5 * conj(arr_new[inds_l...])
+            end
+    
+        end
+        return arr_out
+    end
+end
+
+
+
 
 """
     downsample(arr, new_size)
