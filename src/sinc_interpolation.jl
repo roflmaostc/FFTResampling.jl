e6a3112b3a6eb27a2fa66a920f592c891d11a73b
roflmaostc <fxw+git@mailbox.org>
2021-01-06 14:21:14 +0100

    Rewrite make_hermitian and add helper methods

--- src/sinc_interpolation.jl
+++ src/sinc_interpolation.jl
@@ -12,7 +12,7 @@ This method is based on FFTs and therefore implicitly assumes periodic
 boundaries and a finite frequeny support.
 
 # Examples
-```julia-repl
+```jldoctest
 julia> sinc_interpolate([1.0, 2.0, 3.0, 4.0], 8)
 8-element Array{Float64,1}:
  1.0
@@ -57,94 +57,6 @@ function sinc_interpolate(arr::AbstractArray{T}, new_size) where T<:Real
 end
 
 
-"""
-    make_hermitian(arr)
-
-Takes an array `arr` and appends rows, cols, ... if necessary
-so that `arr` is a hermitian array which preserves Parseval's theorem.
-
-# Examples
-```julia-repl
-julia> FFTInterpolations.make_hermitian([1.0 2.0])
-1×3 Array{Float64,2}:
- 0.5  2.0  0.5
-
-julia> FFTInterpolations.make_hermitian([1.0 2.0; 3.0 4.0])
-3×3 Array{Float64,2}:
- 0.5  1.0  0.0
- 1.5  4.0  1.5
- 0.0  1.0  0.5
-
-julia> FFTInterpolations.make_hermitian([1im 2.0; 3.0im 4.0im; 5.0 6.0im])
-3×3 Array{Complex{Float64},2}:
- 0.0+0.5im  2.0+0.0im  2.5-0.0im
- 0.0+1.5im  0.0+4.0im  0.0-1.5im
- 2.5+0.0im  0.0+6.0im  0.0-0.5im
-```
-"""
-@gg function make_hermitian(arr::AbstractArray{T, N}) where {T, N}
-    N2 = N - 1
-    quote 
-        # if the size is odd, we need to add a slice to that dimension
-        # at this slice we then fill new values, so that `arr` is hermitian
-        mf(x) = x % 2 == 0 ? x +1 : x
-        size_new = map(mf, size(arr))
-        # new array with new size 
-        arr_new = zeros(eltype(arr), size_new)
-        
-        # fill the new array with the old one 
-        same_ind = map(x -> 1:x, size(arr))
-        arr_new[same_ind...] = arr 
-        
-        # copy again to have a out array (might be necessary because of reassigning)
-        arr_out = copy(arr_new)
-        
-        # now modify values at the edges to get hermitian property
-        # and Parsveval's theorem correct
-        for d = 1:$N
-            # @show d
-            # special case when singleton dimension 
-            if size(arr_new)[d] == 1 || size(arr)[d] % 2 == 1
-                continue
-            end
-            
-            # the N2 dimensional loops, loop over this
-            iter_nloops = map(x -> 1:x, collect(size_new))
-            # delete at dimension d because we have only N2 dimensions to loop over
-            deleteat!(iter_nloops, d)
-            
-            # @show iter_nloops
-            @nloops $N2 i (k -> iter_nloops[k]) begin
-                # indices to access the left slice
-                # left slice is the existing slice which will be copied
-                inds_l = convert(Array{Int},collect(@ntuple $N2 i))
-                insert!(inds_l, d, 1)
-                
-                # right slice which will be copied from the left slice
-                inds_r = similar(inds_l)
-                
-                # because of the hermitian property, we reverse the slices
-                for j in 1:length(inds_r)
-                    # in this case, we want to get the right hand slice
-                    if j == d
-                        inds_r[j] = size_new[j]
-                    else
-                        inds_r[j] = size_new[j] - inds_l[j] + 1
-                    end
-                end
-                
-                # assign the complex conjugate 
-                arr_out[inds_l...] = 0.5 * arr_new[inds_l...]
-                arr_out[inds_r...] = 0.5 * conj(arr_new[inds_l...])
-            end
-    
-        end
-        return arr_out
-    end
-end
-
-
-
 
 """
     downsample(arr, new_size)
@@ -154,7 +66,7 @@ This is calculated by cutting a centered frequency window from the frequency
 spectrum and going back to real space with an `ifft`.
 
 # Examples
-```julia-repl
+```jldoctest
 julia> FFTInterpolations.downsample([1.0, 0.0, 1.0, 0.0, 1.0, 0.0], [3])
 3-element Array{Float64,1}:
  0.5
@@ -197,7 +109,7 @@ This method is slow, because of an explicit sum evalulation and
 not a FFT based evaluation.
 
 # Examples
-```julia-repl
+```jldoctest
 julia> sinc_interpolate_sum([1.0, 2.0, 3.0, 4.0], 8)
 8-element Array{Float64,1}:
  1.0
@@ -207,7 +119,7 @@ julia> sinc_interpolate_sum([1.0, 2.0, 3.0, 4.0], 8)
  3.0
  4.1592491794681985
  4.0
- 2.073561544282979
+ 2.0735615442829793
 ```
 """
 function sinc_interpolate_sum(arr, new_size)
--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,74 @@
 """
+    make_hermitian(arr)
+
+Takes an array `arr` and appends rows, cols, ... if necessary
+so that `arr` is a hermitian array which preserves Parseval's theorem.
+
+# Examples
+```jldoctest
+julia> FFTInterpolations.make_hermitian([1.0 2.0])
+1×3 Array{Float64,2}:
+ 0.5  2.0  0.5
+
+julia> FFTInterpolations.make_hermitian([1.0 2.0; 3.0 4.0])
+3×3 Array{Float64,2}:
+ 0.5  1.0  0.0
+ 1.5  4.0  1.5
+ 0.0  1.0  0.5
+
+julia> FFTInterpolations.make_hermitian([1im 2.0; 3.0im 4.0im; 5.0 6.0im])
+3×3 Array{Complex{Float64},2}:
+ 0.0+0.5im  2.0+0.0im  2.5-0.0im
+ 0.0+1.5im  0.0+4.0im  0.0-1.5im
+ 2.5+0.0im  0.0+6.0im  0.0-0.5im
+```
+"""
+function make_hermitian(arr::AbstractArray{T, N}) where {T, N}
+    # if the size is odd, we need to add a slice to that dimension
+    # at this slice we then fill new values, so that `arr` is hermitian
+    mf(x) = x % 2 == 0 ? x +1 : x
+    size_new = map(mf, size(arr))
+    # new array with new size 
+    arr_new = zeros(eltype(arr), size_new)
+    
+    # fill the new array with the old one 
+    same_ind = map(x -> 1:x, size(arr))
+    arr_new[same_ind...] = arr 
+    
+    # copy again to have a out array (might be necessary because of reassigning)
+    arr_out = copy(arr_new)
+    
+    # now modify values at the edges to get hermitian property
+    # and Parsveval's theorem correct
+    for d = 1:N
+        # special case when singleton dimension 
+        if size(arr_new)[d] == 1 || size(arr)[d] % 2 == 1
+            continue
+        end
+    
+        # extract indices represent the left slice we need to copy
+        extract_indices = slice_indices(arr_new, d, 1)
+        y = arr_new[extract_indices...] ./ 2
+        y_rev = reverse_all(y)
+        
+        # assign indices represent the right sliuce we need to assign the
+        # conjugated and halved one
+        assign_indices = slice_indices(arr_new, d, size(arr_out)[d])
+
+        # assign the halved
+        arr_out[extract_indices...] = y
+        # assign the conjugated, halved one
+        arr_out[assign_indices...] = conj.(y_rev)
+    end
+
+    return arr_out
+end
+
+
+
+
+
+"""
     get_indices_around_center(i_in, i_out)
 A function which provides two output indices `i1` and `i2`
 where `i2 - i1 = i_out`
@@ -23,35 +93,42 @@ end
 
 
 """
-    center_extract(arr, new_size)
+    center_extract(arr, new_size_array)
 Extracts a center of an array. 
-`new_size` must be list of sizes indicating the output
+`new_size_array` must be list of sizes indicating the output
 size of each dimension. Centered means that a center frequency
 stays at the center position. Works for even and uneven.
-If `length(new_size) < length(size(arr))` the remaining dimensions
+If `length(new_size_array) < length(ndims(arr))` the remaining dimensions
 are untouched and copied.
 # Examples
-```julia-repl
-julia> center_extract([[1,2] [3, 4]], [1])
+```jldoctest
+julia> FFTInterpolations.center_extract([1 2; 3 4], [1]) 
 1×2 Array{Int64,2}:
- 2  4
-julia> center_extract([[1,2] [3, 4]], [1, 1])
+ 3  4
+
+julia> FFTInterpolations.center_extract([1 2; 3 4], [1, 1])
 1×1 Array{Int64,2}:
 4
+
+julia> FFTInterpolations.center_extract([1 2 3; 3 4 5; 6 7 8], [2 2])
+2×2 Array{Int64,2}:
+ 1  2
+ 3  4
 ```
 """
-function center_extract(arr, index_arrays)
-    index_arrays = collect(index_arrays)
+function center_extract(arr::AbstractArray, new_size_array)
+    new_size_array = collect(new_size_array)
 
     # we construct two lists
     # the reason is, that we don't change higher dimensions which are not 
-    # specified in index_arrays
-    out_indices1 = [get_indices_around_center(size(arr)[x], index_arrays[x]) 
-                    for x = 1:length(index_arrays)]
+    # specified in new_size_array
+    out_indices1 = [get_indices_around_center(size(arr)[x], new_size_array[x]) 
+                    for x = 1:length(new_size_array)]
     
     out_indices1 = [x[1]:x[2] for x = out_indices1]
-
-    out_indices2 = [1:size(arr)[length(out_indices1) + i] for i = (1 + size(index_arrays)[1]):ndims(arr)]
+    
+    # out_indices2 contains just ranges covering the full size of each dimension
+    out_indices2 = [1:size(arr)[i] for i = (1 + length(new_size_array)):ndims(arr)]
     return arr[out_indices1..., out_indices2...]
 end
 
@@ -63,7 +140,7 @@ The convention, where the center is, is the same as the definition
 as for FFT based centered.
 Function works both for even and uneven arrays.
 # Examples
-```julia-repl
+```jldoctest
 julia> center_set!([1, 1, 1, 1, 1, 1], [5, 5, 5])
 6-element Array{Int64,1}:
  1
@@ -93,7 +170,7 @@ end
 Calculate the position of the center frequency.
 Size of the array is `x`
 # Examples
-```julia-repl
+```jldoctest
 julia> center_pos(3)
 2
 julia> center_pos(4)
@@ -115,33 +192,76 @@ Return a `N` dimensional slice (where one dimensions has size 1) of the N-dimens
 It holds `size(out)[dim] == 1`.
 
 # Examples
-```julia-repl
-julia> x = randn((3, 3))
-3×3 Array{Float64,2}:
- -0.925331   1.79456     0.465846
- -0.18492    0.705636    0.328199
-  1.36222   -0.0132336  -0.586589
+```jldoctest
+julia> x = [1 2 3; 4 5 6; 7 8 9]
+3×3 Array{Int64,2}:
+ 1  2  3
+ 4  5  6
+ 7  8  9
 
 julia> FFTInterpolations.slice(x, 2, 2)
-3×1 Array{Float64,2}:
-  1.794557861500336
-  0.7056355732334497
- -0.013233577444161712
+3×1 Array{Int64,2}:
+ 2
+ 5
+ 8
 
 julia> FFTInterpolations.slice(x, 1, 1)
-1×3 Array{Float64,2}:
- -0.925331  1.79456  0.465846
+1×3 Array{Int64,2}:
+ 1  2  3
 ```
 
 """
 function slice(arr::AbstractArray{T, N}, dim::Integer, index::Integer) where {T, N}
-    @assert 1 ≤ dim ≤ N
-
-    inds = fill(1:1, N)
-
-    for (i, v) in enumerate(size(arr))
-        inds[i] = (dim == i ? (index:index) : (1:v))
-    end
-
+    inds = slice_indices(arr, dim, index)
     return arr[inds...]
+end
+
+
+
+function slice_indices(arr::AbstractArray{T, N}, dim::Integer, index::Integer) where {T, N}
+    @assert 1 ≤ dim ≤ N
+    
+    inds = Array{Any}(undef, N)
+    inds = fill!(inds, :)
+    inds[dim] = index:index
+
+    return inds
+end
+
+
+"""
+    reverse_all(arr)
+
+Reverse an array `arr` over all dimensions.
+
+# Examples
+```jldoctest
+julia> FFTInterpolations.reverse_all([1 2 3 4 5])
+1×5 Array{Int64,2}:
+ 5  4  3  2  1
+
+julia> FFTInterpolations.reverse_all([1 2 3 4 5; 7 8 9 10 11])
+2×5 Array{Int64,2}:
+ 11  10  9  8  7
+  5   4  3  2  1
+
+julia> FFTInterpolations.reverse_all([1; 2; 3; 4; 5])
+5-element Array{Int64,1}:
+ 5
+ 4
+ 3
+ 2
+ 1
+```
+"""
+function reverse_all(arr::AbstractArray)
+    return arr[reverse_all_indices(arr)...]
+end
+
+function reverse_all_indices(arr)
+    out = []
+    for i in size(arr)
+        push!(out, i:-1:1)
+    end
+    return out 
 end
