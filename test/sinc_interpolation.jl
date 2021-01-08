74bfb9425380833811552f0996666e1d890b157e
roflmaostc <fxw+git@mailbox.org>
2021-01-08 21:26:52 +0100

    Add more testcases

--- test/sinc_interpolation.jl
+++ test/sinc_interpolation.jl
@@ -77,19 +77,44 @@ end
 
 
 @testset "FFT sinc downsample and upsample together in 2D" begin
+
+
+    function test_2D(in_s, out_s)
+        x = range(-10.0, 10.0, length=in_s[1] + 1)[1:end-1]
+        y = range(-10.0, 10.0, length=in_s[2] + 1)[1:end-1]'
+	    arr = sinc.(sqrt.(x .^2 .+ y .^2))
+	    arr_interp = sinc_interpolate(arr[1:end, 1:end], out_s);
+	    arr_ds = downsample(arr_interp, in_s)
+        @test arr_ds ≈ arr
+    end
+
+    test_2D((128, 128), (150, 150))
+    test_2D((128, 128), (151, 151))
+    test_2D((129, 129), (150, 150))
+    test_2D((129, 129), (151, 151))
+    
+    test_2D((150, 128), (151, 150))
+    test_2D((128, 128), (151, 153))
+    test_2D((129, 128), (150, 153))
+    test_2D((129, 128), (129, 153))
+
+
     x = range(-10.0, 10.0, length=129)[1:end-1]
-	x_exact = range(-10.0, 10.0, length=2049)[1:end-1]
-	y = x'
-	y_exact = x_exact'
-	arr = sinc.(sqrt.(x .^2 .+ y .^2))
-	arr_exact = sinc.(sqrt.(x_exact .^2 .+ y_exact .^2))
-	arr_interp = sinc_interpolate(arr[1:end, 1:end], (131, 131));
-	arr_interp2 = sinc_interpolate(arr[1:end, 1:end], (512, 512));
-	arr_interp3 = sinc_interpolate(arr[1:end, 1:end], (1024, 1024));
-	arr_ds = downsample(arr_interp, (128, 128))
-	arr_ds2 = downsample(arr_interp, (128, 128))
-	arr_ds23 = downsample(arr_interp2, (512, 512))
-	arr_ds3 = downsample(arr_interp, (128, 128))
+    x2 = range(-10.0, 10.0, length=130)[1:end-1]
+    x_exact = range(-10.0, 10.0, length=2049)[1:end-1]
+    y = x'
+    y2 = x2'
+    y_exact = x_exact'
+    arr = sinc.(sqrt.(x .^2 .+ y .^2))
+    arr2 = sinc.(sqrt.(x .^2 .+ y .^2))
+    arr_exact = sinc.(sqrt.(x_exact .^2 .+ y_exact .^2))
+    arr_interp = sinc_interpolate(arr[1:end, 1:end], (131, 131));
+    arr_interp2 = sinc_interpolate(arr[1:end, 1:end], (512, 512));
+    arr_interp3 = sinc_interpolate(arr[1:end, 1:end], (1024, 1024));
+    arr_ds = downsample(arr_interp, (128, 128))
+    arr_ds2 = downsample(arr_interp, (128, 128))
+    arr_ds23 = downsample(arr_interp2, (512, 512))
+    arr_ds3 = downsample(arr_interp, (128, 128))
 
     @test ≈(arr_ds3, arr)
     @test ≈(arr_ds2, arr)
